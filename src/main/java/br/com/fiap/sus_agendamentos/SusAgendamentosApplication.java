eff33315d5e7d164ab79a8de6c9b5c3015c739bc
Fábio Ferreira <fabio_junior1994@hotmail.com>
2025-02-17 23:16:50 -0300

    Inclusão das dependências necessárias para desenvolvimento do projeto.

---
+++ src/main/java/br/com/fiap/sus_agendamentos/SusAgendamentosApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.sus_agendamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SusAgendamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SusAgendamentosApplication.class, args);
+	}
+
+}
