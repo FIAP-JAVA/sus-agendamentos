e3ae859405d849aaae1687c4f5808debdf9038bc
Fábio Ferreira <fabio_junior1994@hotmail.com>
2025-03-01 01:30:00 -0300

    Implementação de validação dos campos obrigatórios de Médicos e Endereço.

--- src/main/java/br/com/fiap/sus_agendamentos/controller/MedicoController.java
+++ src/main/java/br/com/fiap/sus_agendamentos/controller/MedicoController.java
@@ -3,6 +3,7 @@ package br.com.fiap.sus_agendamentos.controller;
 import br.com.fiap.sus_agendamentos.medico.Medico;
 import br.com.fiap.sus_agendamentos.medico.MedicoDTO;
 import br.com.fiap.sus_agendamentos.medico.MedicoRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,7 +20,7 @@ public class MedicoController {
 
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody MedicoDTO medicoDTO) {
+    public void cadastrar(@RequestBody @Valid MedicoDTO medicoDTO) {
         medicoRepository.save(new Medico(medicoDTO));
     }
 
--- src/main/java/br/com/fiap/sus_agendamentos/endereco/EnderecoDTO.java
+++ src/main/java/br/com/fiap/sus_agendamentos/endereco/EnderecoDTO.java
@@ -1,12 +1,26 @@
 package br.com.fiap.sus_agendamentos.endereco;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
 public record EnderecoDTO(
 
+        @NotBlank
         String logradouro,
+
+        @NotBlank
         String bairro,
+
+        @NotBlank
+        @Pattern(regexp = "\\d{8}")
         String cep,
+
+        @NotBlank
         String cidade,
+
+        @NotBlank
         String uf,
+
         String complemento,
         String numero
 
